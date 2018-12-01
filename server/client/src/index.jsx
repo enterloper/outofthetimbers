a8fcca6defd376e3810115b098d2acaf9c50f6f7
Rich Boothe <richardjboothe@gmail.com>
2018-11-30 23:09:57 -0600

    update webpack style loading

--- server/client/src/index.jsx
+++ server/client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import './styles/normalize.css';
+import './styles/app-global.scss';
 
 render(<App />, document.getElementById('app'));
--- server/client/src/layouts/FloatingSideMenu/FloatingSideMenu.jsx
+++ server/client/src/layouts/FloatingSideMenu/FloatingSideMenu.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './styles.scss';
 
 const FloatingSideMenu = ({ children }) => {
   return (
-    <div>
+    <div className="test">
       { children }
     </div>
   );
